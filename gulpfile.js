45923d6a0b6dd33766f6d090f2661b01d35531fe
Matvey Nekrasov Home <metal59@gmail.com>
2020-06-23 21:46:12 +0700

    добавлено авто-создание CNAME в gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,127 @@
-import gulp from "gulp";
-import babel from "gulp-babel";
-import postcss from "gulp-postcss";
-import replace from "gulp-replace";
-import htmlmin from "gulp-htmlmin";
-import terser from "gulp-terser";
-import pimport from "postcss-import";
-import minmax from "postcss-media-minmax";
-import autoprefixer from "autoprefixer";
-import csso from "postcss-csso";
-import sync from "browser-sync";
-import ghpages from "gh-pages"; // deploy
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import postcss from 'gulp-postcss';
+import replace from 'gulp-replace';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import pimport from 'postcss-import';
+import minmax from 'postcss-media-minmax';
+import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import sync from 'browser-sync';
+import ghpages from 'gh-pages';
+import fs from 'fs';
 
 // HTML
-
-export const html = () => {
-    return gulp.src("src/*.html")
+const html = () => {
+    return gulp.src('src/*.html')
         .pipe(htmlmin({
             removeComments: true,
             collapseWhitespace: true,
         }))
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest('dist'))
         .pipe(sync.stream());
 };
 
 // Styles
-
-export const styles = () => {
-    return gulp.src("src/styles/index.css")
+const styles = () => {
+    return gulp.src('src/styles/index.css')
         .pipe(postcss([
             pimport,
             minmax,
             autoprefixer,
             csso,
         ]))
-        .pipe(replace(/\.\.\//g, ""))
-        .pipe(gulp.dest("dist"))
+        .pipe(replace(/\.\.\//g, ''))
+        .pipe(gulp.dest('dist'))
         .pipe(sync.stream());
 };
 
 // Scripts
-
-export const scripts = () => {
-    return gulp.src("src/scripts/index.js", { allowEmpty: true })
+const scripts = () => {
+    return gulp.src('src/scripts/index.js', { allowEmpty: true })
         .pipe(babel({
-            presets: ["@babel/preset-env"]
+            presets: ['@babel/preset-env']
         }))
         .pipe(terser())
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest('dist'))
         .pipe(sync.stream());
 };
 
 // Copy
-
-export const copy = () => {
+const copy = () => {
     return gulp.src([
-        "src/fonts/**/*",
-        "src/images/**/*",
+        'src/fonts/**/*',
+        'src/images/**/*',
     ], {
-        base: "src"
+        base: 'src'
     })
-        .pipe(gulp.dest("dist"))
+        .pipe(gulp.dest('dist'))
         .pipe(sync.stream({
             once: true
         }));
 };
 
 // Paths
-
-export const paths = () => {
-    return gulp.src("dist/*.html")
+const paths = () => {
+    return gulp.src('dist/*.html')
         .pipe(replace(
-            /(<link rel="stylesheet" href=")styles\/(index.css">)/, "$1$2"
+            /(<link rel='stylesheet' href=')styles\/(index.css'>)/, '$1$2'
         ))
         .pipe(replace(
-            /(<script src=")scripts\/(index.js">)/, "$1$2"
+            /(<script src=')scripts\/(index.js'>)/, '$1$2'
         ))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest('dist'));
 };
 
 // Watch
-
-export const watch = () => {
-    gulp.watch("src/*.html", gulp.series(html, paths));
-    gulp.watch("src/styles/**/*.css", gulp.series(styles));
-    gulp.watch("src/scripts/**/*.js", gulp.series(scripts));
+const watch = () => {
+    gulp.watch('src/*.html', gulp.series(html, paths));
+    gulp.watch('src/styles/**/*.css', gulp.series(styles));
+    gulp.watch('src/scripts/**/*.js', gulp.series(scripts));
     gulp.watch([
-        "src/fonts/**/*",
-        "src/images/**/*",
+        'src/fonts/**/*',
+        'src/images/**/*',
     ], gulp.series(copy));
 };
 
 // Server
-
-export const server = () => {
+const server = () => {
     sync.init({
         ui: false,
         notify: false,
         server: {
-            baseDir: "dist"
+            baseDir: 'dist'
         }
     });
 };
 
-export const deploy = () => {
-    return ghpages.publish("dist", () => { });
+// Create CNAME file
+const cname = (cb) => {
+    fs.writeFile('dist/CNAME', 'matveynekrasov.ru', cb);
+}
+
+// Publish
+const publish = (cb) => {
+    ghpages.publish('dist', {
+        branch: 'master'
+    }, cb);
 };
 
-// Default
+// Deploy
+export const deploy = gulp.series(
+    gulp.parallel(
+        html,
+        styles,
+        scripts,
+        copy,
+    ),
+    paths,
+    cname,
+    publish,
+);
 
+// Default
 export default gulp.series(
     gulp.parallel(
         html,
